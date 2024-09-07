7ed44fe5d22a4ca40fccfe07f32d6e02f53c3040
Aditya <104798214+Advirocks82@users.noreply.github.com>
2024-09-07 17:14:09 +0530

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import RegistrationPage from './RegistrationPage';
+import './App.css';  // Import a CSS file for styling
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
       <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<LoginPage/>}> </Route>
-        <Route path='/registration' element= {<RegistrationPage/>}></Route>
-      </Routes>
+        {/* Navigation Bar */}
+        <nav className="navbar">
+          <h1 className="app-title">My Application</h1>
+          <div className="nav-links">
+            <Link to="/registration" className="nav-link">Dashboard</Link>
+            <Link to="/login" className="nav-link">Login</Link>
+          </div>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/registration' element={<RegistrationPage />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
